fb155b8a97baa1ef690eb68ac31140d65fa81b32
jihyoung9912 <jihyoung9912@gmail.com>
2022-07-06 21:36:38 +0900

    FEAT : 3003.py

---
+++ bronze5/2744.py
@@ -0,0 +1,12 @@
+word = input()
+# new = ''
+
+# for i in range(0, len(word)):
+#   if word[i].isupper() == True:
+#     new += word[i].lower()
+#   elif word[i].isupper() == False:
+#     new += word[i].upper()
+
+
+# print(new)
+print(word.swapcase())
