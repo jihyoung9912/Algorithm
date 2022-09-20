e7c087baf7462fe81f147ecd03721cc19bf40f84
김지형 <jihyoung9912@gmail.com>
2022-09-21 03:37:25 +0900

    feat: ex07.py

--- DataStructure/2w/ex07.py
+++ DataStructure/2w/ex07.py
@@ -1,11 +1,8 @@
 def solution(lst):
-    freq = {x:lst.count(x) for x in set(lst)}
+    freq = {x:lst.count(x) for x in set(lst)} #{1: 1, 2: 1, 3: 1, 4: 1, 5: 2}
     if len(freq) == 1 or len(freq) == len(lst):
         return []
     return [x for x in freq if freq[x] == max(freq.values())]
 
 
 print(solution([1, 2, 3, 4, 5, 5])) #[5]
-print(solution([12, 17, 19, 17, 23])) #[17]
-print(solution([26, 37, 26, 37, 91])) #[26, 37]
-print(solution([28, 30, 32, 34, 144])) #[]
