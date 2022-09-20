ff884d7c0c5cfce4bdf8df729f8aec9738363c6b
김지형 <jihyoung9912@gmail.com>
2022-09-21 01:26:35 +0900

    feat : ex04.py

--- DataStructure/2w/ex04.py
+++ DataStructure/2w/ex04.py
@@ -6,7 +6,7 @@ sum_ = 0
 exp = int(math.log10(num))
 
 for i in range(exp, -1, -1):
-    x = num // 10 ** exp
-    sum_ += x
-    num -= x * 10 ** exp
-    print(num)
+    sum_ += num // 10 ** i
+    num = num % 10 ** i
+
+print(sum_)
