591b300e03f7e501f5ac4209a5c89943cd0c4142
김지형 <jihyoung9912@gmail.com>
2022-09-20 16:55:32 +0900

    feat: ex02.py

---
+++ DataStructure/2w/ex02.py
@@ -0,0 +1,15 @@
+import math
+
+BASE = 2
+num_dec = 11
+num_bin = ''
+print(f"Decimal number = {num_dec}")
+
+cnt_iter = int(math.log(num_dec, 2))
+while cnt_iter > 0:
+    num_dec, r = num_dec // BASE, num_dec % BASE
+    cnt_iter -= 1
+    num_bin = str(r) + num_bin
+
+num_bin = str(num_dec) + num_bin
+print(f"Binary number = {num_bin}")
