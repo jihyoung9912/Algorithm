6dc66138ad90cb758f467fd4cc829869dbed2550
김지형 <jihyoung9912@gmail.com>
2022-09-21 15:33:15 +0900

    feat: ex02.py

--- DataStructure/3w/ex02.py
+++ DataStructure/3w/ex02.py
@@ -1,23 +1,15 @@
 class Element:
-    def __init__(self, num=0):
+    def __init__(self, num = 0):
         self.num = num
-
     def __str__(self):
-        return f"Element:{self.num}"
-
+        return f'Element : {self.num}'
     def __repr__(self):
         return str(self)
-
     def __add__(self, other):
-        if not isinstance(other, Element): #other가 Element의 객체인지 확인
-            raise Exception("should not be different type")
-        # Element(10) + 10과 같이 Element의 class가 아닌 값을 예외처리
-
+        if not isinstance(other, Element):
+            raise Exception("should be different type")
         add_ = self.num + other.num
         return Element(add_)
 
-elem = Element(10) + Element(10)
+elem = Element(50) + Element(30)
 print(elem)
-
-# a = Element(10)
-# print(type(a))
