c04495d3547ce7c04e2ec95e96ab8e307a8850e0
김지형 <jihyoung9912@gmail.com>
2022-09-21 15:34:44 +0900

    feat: ex03.py

--- DataStructure/3w/ex03.py
+++ DataStructure/3w/ex03.py
@@ -1,26 +1,20 @@
 class Element:
-    def __init__(self, num=0):
+    def __init__(self, num = 0):
         self.num = num
-
     def __str__(self):
-        return f"Element:{self.num}"
-
+        return f'Element : {self.num}'
     def __repr__(self):
         return str(self)
-
     def __add__(self, other):
         if not isinstance(other, Element):
-            raise Exception("should not be different type")
-
+            raise Exception("should be different type")
         add_ = self.num + other.num
         return Element(add_)
-
     def __sub__(self, other):
         if not isinstance(other, Element):
-            raise Exception("should not be different type")
+            raise Exception("should be different type")
+        sub_ = self.num - other.num
+        return Element(sub_)
 
-        sub = self.num - other.num
-        return Element(sub)
-
-# elem = Element(55) - Element(10)
-# print(elem)
+elem = Element(50) - Element(30)
+print(elem)
