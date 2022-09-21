9dc66025ce816010d8022fbe47036b22988910c2
김지형 <jihyoung9912@gmail.com>
2022-09-21 14:59:28 +0900

    feat: ex01.py

--- DataStructure/3w/ex01.py
+++ DataStructure/3w/ex01.py
@@ -1,13 +1,10 @@
 class Element:
-    def __init__(self, num=0):
+    def __init__(self, num = 0):
         self.num = num
-
-    def __str__(self): #클래스를 print 했을 때의 출력 값 str, repr이 동시에 존재할 경우 str로 호출됨
-        #str 함수를 먼저 찾고, 없으면 repr을 찾는다
-        return f"Element:{self.num}"
-
-    def __repr__(self): #객체에 이름을 지어준다.  메모리값을 표현해주는 함수
+    def __str__(self):
+        return f'Element : {self.num}'
+    def __repr__(self):
         return str(self)
 
-elem = [Element(100), Element(200)]
+elem = [Element(100), Element(10)]
 print(elem)
