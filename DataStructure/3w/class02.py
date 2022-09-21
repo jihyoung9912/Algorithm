5de311d8429e4514c8440219eb9dd18b945a9e78
김지형 <jihyoung9912@gmail.com>
2022-09-21 14:39:20 +0900

    feat: class02.py

--- DataStructure/3w/class02.py
+++ DataStructure/3w/class02.py
@@ -1,10 +1,22 @@
+# class Student:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def __str__(self):
+#         return f"name is: {self.name}, {self.age}"
+#
+# st = Student("JiHyeong", 24)
+# print(st)
+
+
 class Student:
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
     def __str__(self):
-        return f"name is: {self.name}, {self.age}"
+        return f'name is {self.name}, age is {self.age}'
 
-st = Student("JiHyeong", 24)
+st = Student('Name', 24)
 print(st)
