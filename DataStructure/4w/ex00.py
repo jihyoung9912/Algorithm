2cddc070587b3cac2c5b6801388f18b66c5b9717
김지형 <jihyoung9912@gmail.com>
2022-09-28 02:50:09 +0900

    feat: ex00

--- DataStructure/4w/ex00.py
+++ DataStructure/4w/ex00.py
@@ -1,20 +1,57 @@
 class OrderedList:
     def __init__(self):
-        self.elems = []
+        self.arr = []
+
+    def __str__(self):
+        return f"{self.arr}"
+
     def is_empty(self):
-        return not bool(self.elems)
-    def add(self, elem):
-        if not self.elems:
-            self.elems.append(elem)
+        return not bool(self.arr)
+
+    def add(self, num):
+        # return self.arr.append(num)
+        if not self.arr:
+            self.arr.append(num)
             return
         cur = 0
-        while cur < len(self) and self[cur] <= elem:
+        while cur < len(self) and self[cur] <= num:
             cur += 1
-        self.elems.insert(cur, elem)
-    def remove(self, elem):
-        self.elems.remove(elem)
-    def search(self, elem):
+        self.arr.insert(cur, num)
+
+    def __len__(self):
+        return len(self.arr)
+
+    def __getitem__(self, item):
+        return self.arr[item]
+
+    def remove(self, num):
+        return self.arr.remove(num)
+
+    def search(self, num):
+        # return True if num in self.arr else False
         cur = 0
-        while cur < len(self) and self[cur] != elem:
+        while cur < len(self) and self[cur] != num:
             cur += 1
         return False if cur >= len(self) else True
+
+    def index(self, num):
+        return self.arr.index(num) if num in self.arr else False
+
+
+*data, = 53, 17, 34, 23, 15, 43
+# print(data)
+
+o = OrderedList()
+# print(o.is_empty())
+
+for i in data:
+    o.add(i)
+print(o.arr)
+print(o.is_empty())
+print(o)
+
+o.remove(43)
+print(o)
+
+print(o.search(53))
+print(o.index(43))
