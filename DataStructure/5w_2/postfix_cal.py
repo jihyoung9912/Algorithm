7abbb92d4a8f4a3ba318e15efa7ef840ec166df2
김지형 <jihyoung9912@gmail.com>
2022-10-07 21:37:30 +0900

    feat: infix.py

--- DataStructure/5w_2/postfix_cal.py
+++ DataStructure/5w_2/postfix_cal.py
@@ -7,10 +7,16 @@ class Postfix:
         self.expr = expr
 
     def evaluate(self):
-        stack = Stack(len(self.expr))
+        # stack = Stack(len(self.expr))
+        stack = len(self.expr)
         ret = 0
+        list_ = []
+        for i in self.expr:
+            if i != ' ':
+                list_.append(i)
+                self.cal(list_)
 
-        return ret
+        return list_
 
     def cal(self, tok_1, tok_2, op):
         if op == "+":
@@ -26,11 +32,15 @@ class Postfix:
     def __str__(self):
         return self.expr
 
+    def __len__(self):
+        return len(self.expr)
+
 
 if __name__ == "__main__":
     expr = "4 10 2 + * 7 -"
 
     postfix = Postfix(expr)
     print("Expression:", postfix)
+    print(postfix.evaluate())
     # res = postfix.evaluate()
     # print("Caculation:", res)
--- DataStructure/5w_2/stacks.py
+++ DataStructure/5w_2/stacks.py
@@ -1,54 +1,51 @@
+from array import Array
+
+
 class Stack:
-    def __init__(self):
-        self.items = []
+    CAPACITY = 10
+
+    def __init__(self, capacity=CAPACITY):
+        self.arr = Array(capacity)
+        self.capacity = capacity
+        self.top = -1
+
+    def is_full(self):
+        return self.top >= self.capacity
 
     def is_empty(self):
-        return not self.items
+        return self.top < 0
 
     def push(self, elem):
-        self.items.append(elem)
+        if self.is_full():
+            raise Exception("stack is full")
+
+        self.top += 1
+        self.arr[self.top] = elem
 
     def pop(self):
         if self.is_empty():
-            raise Exception("stack is empty.")
-        self.items.pop()
+            raise Exception("stack is empty")
+
+        self.arr[self.top] = None
+        self.top -= 1
 
     def peek(self):
         if self.is_empty():
-            raise Exception("stack is empty.")
-        return self.items[-1]
+            raise Exception("stack is empty")
+
+        return self.arr[self.top]
+
+    def __len__(self):
+        return self.top + 1
 
     def __iter__(self):
         pos = 0
         while pos < len(self):
-            yield self.items[pos]
+            yield self.arr[pos]
             pos += 1
 
-    def __len__(self):
-        return len(self.items)
-
     def __str__(self):
-        return str(self.items)
+        return str(self.arr)
 
-if __name__ == "__main__":
-    stack = Stack()
-    print("Length:", len(stack))
-    print("Is Empty:", stack.is_empty())
 
-    N = 4
-    print("Push from 1 to", N)
-    for i in range(1, N + 1):
-        print("Push:", i)
-        stack.push(i)
-        print("Len:", len(stack), "Stack:", stack)
-        print("Peek:", stack.peek())
-    print("Is Empty:", stack.is_empty())
-    for i in stack:
-        print("Element:", i)
-    print()
-    for i in range(N):
-        print("Peek and Pop: ", stack.peek())
-        stack.pop()
-    print("Stack:", stack)
-    print("Length:", len(stack))
-    print("Is Empty:", stack.is_empty())
+
