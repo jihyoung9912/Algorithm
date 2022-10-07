fb003b275d31d0af4e170afa6e0b810b9db0882e
김지형 <jihyoung9912@gmail.com>
2022-10-08 00:30:30 +0900

    feat: postifx_cal.py

--- DataStructure/5w_2/postfix_cal.py
+++ DataStructure/5w_2/postfix_cal.py
@@ -1,5 +1,6 @@
 from stacks import Stack
 
+
 class Postfix:
     OPS = "+", "-", "*", "/"
 
@@ -7,16 +8,22 @@ class Postfix:
         self.expr = expr
 
     def evaluate(self):
-        # stack = Stack(len(self.expr))
-        stack = len(self.expr)
+        stack = Stack(len(self.expr))
         ret = 0
-        list_ = []
-        for i in self.expr:
-            if i != ' ':
-                list_.append(i)
-                self.cal(list_)
+        tmp = expr.split(' ')
 
-        return list_
+        for i in tmp:
+            if i in self.OPS:
+                tok_2 = int(stack.peek())
+                stack.pop()
+                tok_1 = int(stack.peek())
+                stack.pop()
+                ret = self.cal(tok_1, tok_2, i)
+                stack.push(ret)
+
+            elif i not in self.OPS:
+                stack.push(i)
+        return ret
 
     def cal(self, tok_1, tok_2, op):
         if op == "+":
@@ -32,15 +39,11 @@ class Postfix:
     def __str__(self):
         return self.expr
 
-    def __len__(self):
-        return len(self.expr)
-
 
 if __name__ == "__main__":
     expr = "4 10 2 + * 7 -"
 
     postfix = Postfix(expr)
     print("Expression:", postfix)
-    print(postfix.evaluate())
-    # res = postfix.evaluate()
-    # print("Caculation:", res)
+    res = postfix.evaluate()
+    print("Caculation:", res)
