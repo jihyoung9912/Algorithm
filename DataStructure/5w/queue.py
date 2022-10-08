4a4a4733ee575d93c32c6ca62ca4161a5e5650ff
김지형 <jihyoung9912@gmail.com>
2022-10-08 21:00:46 +0900

    feat: queue.py

---
+++ DataStructure/5w/queue.py
@@ -0,0 +1,74 @@
+from array import Array
+
+
+class Queue:
+    CAPACITY = 10
+
+    def __init__(self, capacity=CAPACITY):
+        self.arr = Array(capacity)
+        self.capacity = capacity
+        self.front = self.rear = -1
+
+    def is_full(self):
+        return self.rear - self.front == self.capacity - 1
+
+    def is_empty(self):
+        return self.front == self.rear == -1
+
+    def enqueue(self, elem):
+        if self.is_full():
+            raise Exception("Queue is full")
+
+        self.rear += 1
+        self.arr[self.rear] = elem
+
+        if self.front == -1:
+            self.front = 0
+
+    def dequeue(self):
+        if self.is_empty():
+            raise Exception("Queue is empty")
+
+        self.arr[self.front] = None
+        self.front += 1
+
+        if self.front > self.rear:
+            self.front = self.rear = -1
+
+    def peek(self):
+        return self.arr[self.front]
+
+    def __len__(self):
+        return self.rear + 1
+
+    def __iter__(self):
+        pos = 0
+        while pos < len(self):
+            yield self.arr[pos]
+            pos += 1
+
+    def __str__(self):
+        return str(self.arr)
+
+
+if __name__ == "__main__":
+    N = 5
+    q = Queue(N)
+    print("Length:", len(q))
+    print("Empty:", q.is_empty())
+    print("Enqueue 1-", N)
+
+    for i in range(1, N + 1):
+        q.enqueue(i)
+        print("Peeking:", q.peek())
+        print("Queue =", q)
+
+    print("Length:", len(q))
+    print("Empty:", q.is_empty())
+    while not q.is_empty():
+        print("Peeking:", q.peek())
+        q.dequeue()
+        print("Queue =", q)
+
+    print("Length:", len(q))
+    print("Empty:", q.is_empty())
