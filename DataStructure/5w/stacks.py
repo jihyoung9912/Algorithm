c6f9ac054355c2fdb2508eb19f8bd987743ac8ba
김지형 <jihyoung9912@gmail.com>
2022-10-08 20:08:25 +0900

    feat: stacks.py

--- DataStructure/5w/stacks.py
+++ DataStructure/5w/stacks.py
@@ -1,23 +1,67 @@
-class Array:
-    CAPACITY = 10
+from array import Array
 
-    def __init__(self, capacity=CAPACITY):
-        self.items = [None] * capacity
-
-    def __len__(self):
-        return len(self.items)
-
-    def __str__(self):
-        return str(self.items)
-
-    def __getitem__(self, index):
-        return self.items[index]
-
-    def __setitem__(self, index, item):
-        self.items[index] = item
 
 class Stack:
     CAPACITY = 10
 
     def __init__(self, capacity=CAPACITY):
         self.arr = Array(capacity)
+        self.capacity = capacity
+        self.top = -1
+
+    def is_full(self):
+        return len(self) >= self.capacity
+
+    def is_empty(self):
+        return not len(self)
+
+    def push(self, elem):
+        if self.is_full():
+            raise Exception("Stack is full")
+        self.top += 1
+        self.arr[self.top] = elem
+
+    def pop(self):
+        if self.is_empty():
+            raise Exception("Stack is empty")
+
+        self.arr[self.top] = None
+        self.top -= 1
+
+    def peek(self):
+        if self.is_empty():
+            raise Exception("Stack is empty")
+        return self.arr[self.top]
+
+    def __len__(self):
+        return self.top + 1
+
+    def __iter__(self):
+        pos = 0
+        while pos < len(self):
+            yield self.arr[pos]
+            pos += 1
+
+    def __str__(self):
+        return str(self.arr)
+
+N = 4
+stack = Stack(N)
+print("Length:", len(stack))
+print("Is Empty:", stack.is_empty())
+print("Push from 1 to", N)
+for i in range(1, N + 1):
+    print("Push:", i)
+    stack.push(i)
+    print("Stack:", stack)
+    print("Peek:", stack.peek())
+print("Is Empty:", stack.is_empty())
+for i in stack:
+    print("Element:", i)
+print()
+for i in range(N):
+    print("Peek and Pop: ", stack.peek())
+    stack.pop()
+    print("Stack:", stack)
+print("Length:", len(stack))
+print("Is Empty:", stack.is_empty())
