68e37f18d4f772a6ab1bc0c05bfb19c9f95ef4bd
김지형 <jihyoung9912@gmail.com>
2022-10-09 21:41:39 +0900

    feat : polynomial_mul.py

--- DataStructure/4w_2/polynomial_mul.py
+++ DataStructure/4w_2/polynomial_mul.py
@@ -1,38 +1,26 @@
 import copy
 
+
 class Polynomial:
-    def __init__(self, degree = 0):
+    """Polynomial using array."""
+
+    def __init__(self, degree):
         self.degree = degree
         self.coef = [0] * (self.degree + 1)
 
-    def __str__(self):
-        ret = ""
-        for coef, exp in [
-                             (self.coef[i], i) for i in range(self.degree + 1) if self.coef[i] != 0
-                         ] [
-                         ::-1
-                         ]:
-            ret += f"({coef})x^{exp} + "
-
-        return f"{ret}\b\b"
-
-    def attach(self, coef=0, exp=0):
-        self.coef[exp] = coef
-        return self
-
     def get_lead_exp(self):
-        i = len(self.coef) - 1
-        while i >= 0 and self.coef[i] == 0:
-            i -= 1
-
-        if i < 0:
-            raise Exception("Failed to get_lead_exp.")
-        return i
+        for i in range(self.degree, -2, -1):
+            if self.coef[i] != 0:
+                return i
+            elif i <= -1:
+                raise Exception("Failed to get_lead_exp")
 
     def evaluate(self, x):
-        return sum(
-            (coef * (x**exp) for exp, coef in enumerate(self.coef) if coef != 0)
-        )
+        sum_ = 0
+        for i in range(0, self.degree + 1):
+            if self.coef[i] != 0:
+                sum_ += x ** i * self.coef[i]
+        return sum_
 
     def get_coef(self, exp):
         return self.coef[exp]
@@ -44,12 +32,22 @@ class Polynomial:
         for i in range(len(self.coef)):
             self.coef[i] = 0
 
+    def attach(self, coef, exp):
+        self.coef[exp] = coef
+        return self
+
     def remove(self, exp):
         self.coef[exp] = 0
 
+    def __str__(self):
+        poly = ''
+        for i in range(self.degree, -1, -1):
+            if self.coef[i] != 0:
+                poly += str(f"({self.coef[i]})x^{i} + ")
+        return poly + "\b\b"
+
     def __add__(self, other):
         poly = Polynomial(max(self.degree, other.degree))
-
         while not self.is_zero() or not other.is_zero():
             exp_01 = self.get_lead_exp()
             exp_02 = other.get_lead_exp()
@@ -75,7 +73,7 @@ class Polynomial:
 
         while not self.is_zero():
             exp_01 = self.get_lead_exp()
-            tmp = copy.deepcopy(other)  #깊은 복사 얇은 복사
+            tmp = copy.deepcopy(other)
             while not tmp.is_zero():
                 exp_02 = tmp.get_lead_exp()
                 poly.attach(self.get_coef(exp_01) * tmp.get_coef(exp_02), exp_01 + exp_02)
@@ -83,16 +81,19 @@ class Polynomial:
             self.remove(exp_01)
         return poly
 
-if __name__ == "__main__":
 
+
+
+
+
+if __name__ == "__main__":
     poly1 = Polynomial(2)
     poly1.attach(2, 2).attach(3, 1).attach(1, 0)
     print("poly1 =\n", poly1)
-
     poly2 = Polynomial(1)
     poly2.attach(3, 1).attach(-2, 0)
     print("poly2 =\n", poly2)
-
+    print()
     poly = poly1 * poly2
     print()
     print("poly1 * poly2 =\n", poly)
