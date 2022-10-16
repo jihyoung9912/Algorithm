1098feaf4e780b5e1e2eceb1f3aa0475214a5518
김지형 <jihyoung9912@gmail.com>
2022-10-17 05:12:26 +0900

    feat: arrays.py

---
+++ python/DataStructure/3w_2/arrays.py
@@ -0,0 +1,32 @@
+class Array:
+    def __init__(self, cap=10):
+        self.cap = cap
+        self.list = [None] * cap
+
+    def __str__(self):
+        return f"{self.list}"
+
+    def __len__(self):
+        return len(self.list)
+
+    def __setitem__(self, key, value):
+        self.list[key] = value
+
+    def __getitem__(self, item):
+        return self.list[item]
+
+
+if __name__ == "__main__":
+    arr = Array(5)
+
+    for i in range(len(arr)):
+        arr[i] = i
+
+    print(arr)
+    index = 3
+    print(f"arr[{index}] = {arr[index]}")
+
+    for i in arr:
+        print(id(i), i)
+
+    print(sum(arr)) 
