9551927af328028d2c9f77f24fe4e3259f1137ca
김지형 <jihyoung9912@gmail.com>
2022-10-17 04:29:47 +0900

    feat: binary.py:

--- python/DataStructure/3w/binary.py
+++ python/DataStructure/3w/binary.py
@@ -1,100 +1,85 @@
-class Binary():
+class Binary:
     def __init__(self, data):
         self.data = data
 
     def __str__(self):
-        return ''.join(str(i) for i in data)
+        ans = ''
+        for i in self.data:
+            ans += str(i)
+        return ans
 
     def __len__(self):
         return len(self.data)
 
+    def __getitem__(self, item):
+        return self.data[item]
+
     def __add__(self, other):
-        self.data = self.data[::-1]
-        other.data = other.data[::-1]
-        add_bin = [0 for i in range(max(len(self.data), len(other.data)))]
+        if not isinstance(other, Binary):
+            raise Exception("Please add Binary type")
+
+        if len(self.data) > len(other.data):
+            return self.bin(self.data, other.data)
+        else:
+            return self.bin(other.data, self.data)
+
+    def bin(self, first_bin, second_bin):
+
+        ans = ""
         carry = 0
 
-        for i in range(min(len(self.data), len(other.data))):
-            if self.data[i] == 0 and other.data[i] == 0:
-                if carry == 1:
-                    carry = 0
-                    add_bin[i] = 1
-                else:
-                    carry = 0
-                    add_bin[i] = 0
+        while second_bin:
+            if carry == 0:
+                num = first_bin.pop() + second_bin.pop()
+            else:
+                num = first_bin.pop() + second_bin.pop() + 1
+                carry -= 1
+            if num == 3:
+                carry += 1
+                ans = "1" + ans
+            elif num == 2:
+                carry += 1
+                ans = "0" + ans
+            elif num == 1:
+                ans = "1" + ans
+            elif num == 0:
+                ans = "0" + ans
 
-            elif (self.data[i] == 1 and other.data[i] == 0) or (other.data[i] == 1 and self.data[i] == 0):
-                if carry == 1:
-                    add_bin[i] = 0
-                    carry = 1
-                else:
-                    carry = 0
-                    add_bin[i] = 1
-
-            elif self.data[i] == 1 and other.data[i] == 1:
-                if carry == 1:
-                    add_bin[i] = 1
-                    carry = 1
-                else:
-                    carry = 1
-                    add_bin[i] = 0
-
-        tmp = 1
-        if len(self.data) < len(other.data):
-            tmp = 0
-
-        if tmp == 1:
-            for i in range(min(len(self.data), len(other.data)), max(len(self.data), len(other.data))):
-                if self.data[i] == 0 and carry == 0:
-                    carry = 0
-                    add_bin[i] = 1
-
-                elif (self.data[i] == 1 and carry == 0) or (carry == 1 and self.data[i] == 0):
-                    if carry == 1:
-                        add_bin[i] = 1
-                        carry = 0
-                    else:
-                        carry = 0
-                        add_bin[i] = 1
-
-                elif self.data[i] == 1 and carry == 1:
-                    add_bin[i] = 0
-                    carry = 1
-
-        else:
-            for i in range(min(len(self.data), len(other.data)), max(len(self.data), len(other.data))):
-                if carry == 0 and other.data[i] == 0:
-                    carry = 0
-                    add_bin[i] = 1
-
-                elif (carry == 1 and other.data[i] == 0) or (other.data[i] == 1 and carry == 0):
-                    if carry == 1:
-                        add_bin[i] = 1
-                        carry = 0
-                    else:
-                        carry = 0
-                        add_bin[i] = 1
-
-                elif carry == 1 and other.data[i] == 1:
-                    add_bin[i] = 0
-                    carry = 1
+        while first_bin:
+            if carry == 1:
+                num = first_bin.pop() + 1
+                carry -= 1
+            else:
+                num = first_bin.pop()
+            if num == 3:
+                carry += 1
+                ans = "1" + ans
+            elif num == 2:
+                carry += 1
+                ans = "0" + ans
+            elif num == 1:
+                ans = "1" + ans
+            elif num == 0:
+                ans = "0" + ans
         if carry == 1:
-            add_bin.append(1)
-        add_list = add_bin[:: -1]
-        return ''.join(str(i) for i in add_list)
+            ans = "1" + ans
+
+        return ans
 
 
 *data, = "110101011"
 *data, = map(int, data)
 b1 = Binary(data)
 print(b1)  # 110101011
-print(len(b1))
+print(len(b1))  # 9
 
 *data, = "110111"
 *data, = map(int, data)
 b2 = Binary(data)
-print(b2)
-print(len(b2))
+print(b2)  # 110111
+print(len(b2))  # 6
 
 b = b1 + b2
-print(b)
+print(b)  # 111100010
+print(len(b))  # 9
+print(b[4]) # 0
