525ed3b8db2436d3d3f9538917212bdfa8af4c5f
김지형 <jihyoung9912@gmail.com>
2022-10-20 17:40:58 +0900

    feat: tree.py

---
+++ python/DataStructure/7w_2/lcrs_tree.py
@@ -0,0 +1,52 @@
+from stack import Stack
+
+
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    def build(self, sexpr):
+        stack = Stack()
+        raise NotImplemented
+
+
+class TreeNode:
+    def __init__(self, elem):
+        self.elem = elem
+        self.left_child = None
+        self.right_sibling = None
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return f"{self.elem}"
+
+sexpr = "( A ( B ( E ( K L ) F ) C ( G ) D ( H ( M ) I J ) ) )"
+sexpr = sexpr.split()
+tree = Tree()
+tree.build(sexpr)
+root = tree.root
+print(root)
+b = root.left_child
+print(b)
+e = b.left_child
+print(e)
+k = e.left_child
+print(k)
+l = k.right_sibling
+print(l)
+f = e.right_sibling
+print(f)
+c = b.right_sibling
+print(c)
+d = c.right_sibling
+print(d)
+g = c.left_child
+print(g)
+h = d.left_child
+print(h)
+i = h.right_sibling
+print(i)
+j = i.right_sibling
+print(j)
