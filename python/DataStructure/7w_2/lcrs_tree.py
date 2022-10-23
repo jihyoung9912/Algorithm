256a9572416fd089dd0819c94a2f2bffcf8e62e9
김지형 <jihyoung9912@gmail.com>
2022-10-23 21:49:06 +0900

    feat: icrs_tree.py

--- python/DataStructure/7w_2/lcrs_tree.py
+++ python/DataStructure/7w_2/lcrs_tree.py
@@ -22,31 +22,31 @@ class TreeNode:
     def __str__(self):
         return f"{self.elem}"
 
-sexpr = "( A ( B ( E ( K L ) F ) C ( G ) D ( H ( M ) I J ) ) )"
-sexpr = sexpr.split()
-tree = Tree()
-tree.build(sexpr)
-root = tree.root
-print(root)
-b = root.left_child
-print(b)
-e = b.left_child
-print(e)
-k = e.left_child
-print(k)
-l = k.right_sibling
-print(l)
-f = e.right_sibling
-print(f)
-c = b.right_sibling
-print(c)
-d = c.right_sibling
-print(d)
-g = c.left_child
-print(g)
-h = d.left_child
-print(h)
-i = h.right_sibling
-print(i)
-j = i.right_sibling
-print(j)
+# sexpr = "( A ( B ( E ( K L ) F ) C ( G ) D ( H ( M ) I J ) ) )"
+# sexpr = sexpr.split()
+# tree = Tree()
+# tree.build(sexpr)
+# root = tree.root
+# print(root)
+# b = root.left_child
+# print(b)
+# e = b.left_child
+# print(e)
+# k = e.left_child
+# print(k)
+# l = k.right_sibling
+# print(l)
+# f = e.right_sibling
+# print(f)
+# c = b.right_sibling
+# print(c)
+# d = c.right_sibling
+# print(d)
+# g = c.left_child
+# print(g)
+# h = d.left_child
+# print(h)
+# i = h.right_sibling
+# print(i)
+# j = i.right_sibling
+# print(j)
