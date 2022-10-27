cb28de457da08b550db85c1901fead3c414789de
김지형 <jihyoung9912@gmail.com>
2022-10-28 00:00:37 +0900

    fix: lcrs_tree.py

--- python/DataStructure/7w_2/lcrs_tree.py
+++ python/DataStructure/7w_2/lcrs_tree.py
@@ -6,7 +6,42 @@ class Tree:
         self.root = None
 
     def build(self, sexpr):
-        raise NotImplemented
+        stack = Stack()
+        it = iter(sexpr)
+
+        root = None
+        while stack.is_empty() or it:
+            try:
+                token = next(it)
+            except StopIteration:
+                break
+
+            if token != ")":
+                stack.push(TreeNode(token))
+                continue
+
+            prev = None
+            while stack.peek().elem != "(":
+                node = stack.peek()
+                stack.pop()
+                node.right_sibling = prev
+                prev = node
+
+            stack.pop()
+
+            if stack.is_empty():
+                root = prev
+                continue
+
+            root = stack.peek()
+            root.left_child = prev
+            stack.pop()
+            stack.push(root)
+
+        if not stack.is_empty():
+            raise Exception("expression is wrong.")
+
+        self.root = root
 
 
 class TreeNode:
@@ -24,30 +59,29 @@ class TreeNode:
 
 sexpr = "( A ( B ( E ( K L ) F ) C ( G ) D ( H ( M ) I J ) ) )"
 sexpr = sexpr.split()
-print(sexpr)
-# tree = Tree()
-# tree.build(sexpr)
-# root = tree.root
-# print(root)
-# b = root.left_child
-# print(b)
-# e = b.left_child
-# print(e)
-# k = e.left_child
-# print(k)
-# l = k.right_sibling
-# print(l)
-# f = e.right_sibling
-# print(f)
-# c = b.right_sibling
-# print(c)
-# d = c.right_sibling
-# print(d)
-# g = c.left_child
-# print(g)
-# h = d.left_child
-# print(h)
-# i = h.right_sibling
-# print(i)
-# j = i.right_sibling
-# print(j)
+tree = Tree()
+tree.build(sexpr)
+root = tree.root
+print(root)
+b = root.left_child
+print(b)
+e = b.left_child
+print(e)
+k = e.left_child
+print(k)
+l = k.right_sibling
+print(l)
+f = e.right_sibling
+print(f)
+c = b.right_sibling
+print(c)
+d = c.right_sibling
+print(d)
+g = c.left_child
+print(g)
+h = d.left_child
+print(h)
+i = h.right_sibling
+print(i)
+j = i.right_sibling
+print(j)
