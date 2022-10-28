6bb8c27dfcb3f68c0d9ea11f86bcea78bab3000a
김지형 <jihyoung9912@gmail.com>
2022-10-28 21:01:18 +0900

    feat: tree 이론 관련 주석 추가

--- python/DataStructure/7w_2/lcrs_tree.py
+++ python/DataStructure/7w_2/lcrs_tree.py
@@ -85,3 +85,19 @@ i = h.right_sibling
 print(i)
 j = i.right_sibling
 print(j)
+
+# degree : 자식노드 개수
+# depth : root node에서 나까지
+# height : 나에서 leaf node까지
+# degree of tree : 최대 자식노드 개수 (degree 최대값)
+# size of tree : tree를 구성하고 있는 node 개수
+# height of tree : 최대 나에서 leaf node까지
+# leaf node : degree가 0인 node (자식이 없는 node)
+# internal node : degree가 0 이상인 node (자식을 가진 node)
+# parent : 부모
+# child : 자식
+# sibling : 형제
+# ancestor : root node까지의 path에 존재하는 node들
+# descendent : 자손(leaf node까지), 자식과 헷갈리지 말 것
+# level : node가 존재하는 층
+# waisting memory
