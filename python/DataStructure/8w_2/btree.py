23d4822252e1900a3c6b2f469b52bdb580be0a9f
김지형 <jihyoung9912@gmail.com>
2022-10-31 12:54:40 +0900

    feat: stack, array 세팅

--- python/DataStructure/8w_2/btree.py
+++ python/DataStructure/8w_2/btree.py
@@ -1,3 +1,40 @@
+from stack import Stack
+
+
+class TreeNode:
+    def __init__(self, elem):
+        self.elem = elem
+        self.left_child = None
+        self.right_child = None
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return f"{self.elem}"
+
+
+class BTree:
+    def __init__(self):
+        self.root = None
+
+    def build(self, sexpr):
+        stack = Stack()
+        raise NotImplemented
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Tree에서 lcrs의 경우 siblings의 순서는 전혀 상관 없음
 # Complete binary tree? 꽉 차진 않지만 순서대로 봤을 때 빠진 곳이 없음. 중간을 뛰어넘지 않고 순서대로 배열된 경우.
