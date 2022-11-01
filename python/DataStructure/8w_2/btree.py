d7ba0941bd0efd713034fce76c18b509c91ee93b
김지형 <jihyoung9912@gmail.com>
2022-11-01 19:20:30 +0900

    feat: btree

--- python/DataStructure/8w_2/btree.py
+++ python/DataStructure/8w_2/btree.py
@@ -1,4 +1,34 @@
-from stack import Stack
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return not self.items
+
+    def push(self, elem):
+        self.items.append(elem)
+
+    def pop(self):
+        if self.is_empty():
+            raise Exception("stack is empty.")
+        self.items.pop()
+
+    def peek(self):
+        if self.is_empty():
+            raise Exception("stack is empty.")
+        return self.items[-1]
+
+    def __iter__(self):
+        pos = 0
+        while pos < len(self):
+            yield self.items[pos]
+        pos += 1
+
+    def __len__(self):
+        return len(self.items)
+
+    def __str__(self):
+        return str(self.items)
 
 
 class TreeNode:
@@ -20,21 +50,76 @@ class BTree:
 
     def build(self, sexpr):
         stack = Stack()
-        raise NotImplemented
+        s_stack = Stack()
+        it = iter(sexpr)
+        root = None
+        while stack.is_empty() or it:
+            try:
+                token = next(it)
+            except StopIteration:
+                break
+
+            if token != ")":
+                stack.push(TreeNode(token))
+                continue
+
+            prev = None
+            while stack.peek().elem != "(":
+                node = stack.peek()
+                stack.pop()
+                prev = node
+                s_stack.push(prev)
+
+            stack.pop()
+
+            if stack.is_empty():
+                root = prev
+                continue
+
+            root = stack.peek()
+            if not s_stack.is_empty():
+                if s_stack.peek().elem != "#":
+                    root.left_child = s_stack.peek()
+                    s_stack.pop()
+                else:
+                    s_stack.pop()
+
+                if s_stack.peek().elem != "#":
+                    root.right_child = s_stack.peek()
+                    s_stack.pop()
+                else:
+                    s_stack.pop()
+
+            stack.pop()
+            stack.push(root)
+
+        if not stack.is_empty():
+            raise Exception("expression is wrong.")
+
+        self.root = root
 
 
+if __name__ == "__main__":
+    sexpr = "( 30 ( 5 ( 2 # ) 40 ( # 80 ) ) )".split()
+    tree = BTree()
+    tree.build(sexpr)
+    root = tree.root
+    print(root)
+    print(root.left_child)
+    print(root.left_child.left_child)
+    print(root.left_child.right_child)
+    print(root.right_child)
+    print(root.right_child.left_child)
+    print(root.right_child.right_child)
+    print()
 
-
-
-
-
-
-
-
-
-
-
-
+    sexpr = "( 30 )".split()
+    tree = BTree()
+    tree.build(sexpr)
+    root = tree.root
+    print(root)
+    print(root.left_child)
+    print(root.right_child)
 
 # Tree에서 lcrs의 경우 siblings의 순서는 전혀 상관 없음
 # Complete binary tree? 꽉 차진 않지만 순서대로 봤을 때 빠진 곳이 없음. 중간을 뛰어넘지 않고 순서대로 배열된 경우.
