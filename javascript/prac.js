20c638b8dfe9e4b8332de5719cb4e6a777feb015
김지형 <jihyoung9912@gmail.com>
2023-08-03 20:00:10 -0700

    fix: gitignore

--- javascript/prac.js
+++ javascript/prac.js
@@ -1,16 +1,1 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-let input = [];
-
-rl.on('line',  (line) => {
-    input.push(line);
-    rl.close();
-}).on('close',  () => {
-    console.log(input)
-    process.exit();
-});
+console.log('asd');
